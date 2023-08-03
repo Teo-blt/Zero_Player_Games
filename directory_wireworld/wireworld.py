ade3fb0b16b71dc0aad9118be4a29add5bc7c58e
‘Téo <Teo.Bulteau@wi6labs.com>
2023-08-03 11:14:45 +0200

    Fix : wireworld

--- directory_wireworld/wireworld.py
+++ directory_wireworld/wireworld.py
@@ -2,22 +2,22 @@
 # -*- coding: utf-8 -*-
 # =============================================================================
 # Created By  : Bulteau Téo
-# Created Date: August 1 16:30:00 2023
+# Created Date: August 2 11:30:00 2023
 # For Wi6labs, all rights reserved
 # =============================================================================
 """The Module Has Been Build try zero player games"""
 # =============================================================================
 # Imports
+import main
+import matplotlib
+import numpy as np
+import tkinter as tk
+import matplotlib.pyplot as plt
 from tkinter import *
 from tkinter import ttk
-import matplotlib
-import tkinter as tk
-import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib import animation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import main
-import threading
-
 
 # ============================================================================
 
@@ -26,31 +26,69 @@ class Application(Tk):
     def __init__(self):
         Tk.__init__(self)  # Initialisation of the first window
         self.title("Wireworld")
-        self.color = {0: "black", 1: "yellow", 2: "blue", 3: "red"}
-        self.size = (10, 10)
+        self.color = ["black", "yellow", "blue", "red"]
+        self.step = 0
+        self.OFF = 0
+        self.CONDUCTOR = 1
+        self.ELECTRON_HEAD = 2
+        self.ELECTRON_TAIL = 3
+        self.size = (20, 20)
         self.pixel_start = (75, 72)
         self.pixel_end = (540, 534)
         self.data = np.zeros(self.size)
         self.data_update = np.zeros(self.size)
+        self.step_entry = ttk.Entry()
         self.fig = matplotlib.figure.Figure
         self.ax = matplotlib.axes
         self.canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg
 
+        # Animation settings
+        self.paused = 1
+        self.interval = 100
+        self.im = matplotlib.image.AxesImage
+        self.anim = animation.FuncAnimation
+        self.cmap = colors.ListedColormap(self.color)  # ["black", "yellow", "blue", "red"]
+        self.norm = colors.BoundaryNorm([0, 1, 2, 3, 4], self.cmap.N)  # number of color HERE
+
         # Create widgets
         self.start_wireworld()
 
-    def foo(self):
-        self.plot()
-        threading.Timer(0.5, self.foo).start()
+    def animate(self, i: int):
+        """
+        animate is the animation function
+        """
+        if self.paused:
+            pass
+        else:
+            self.upload_entry()
+            self.im.set_data(self.data)
+            self.data = self.update_data()
 
     def plot(self):
-        self.ax.clear()  # clear axes from previous plot
-        self.update_plt()
-        self.canvas.draw()
+        """
+        plot is a function to advance of one step in the simulation
+        """
+        self.upload_entry()
+        self.data = self.update_data()
+        self.im.set_data(self.data)
+
+    def upload_entry(self):
+        """
+        upload_entry is a function to add + 1 to the step counter
+        """
+        self.step -= -1
+        self.step_entry.delete(0, 2000)
+        self.step_entry.insert(0, "Step : " + str(self.step))
+
+    def toggle_pause(self):
+        """
+        toggle_pause is a function to start/stop the animation
+        """
+        self.paused = not self.paused
 
     def start_wireworld(self):
         """
-        start_wireworld is the main script of Wireworld
+        start_wireworld is the main script of Forest fire
         """
         matplotlib.use('TkAgg')
         self.wm_title("Wireworld")
@@ -65,10 +103,12 @@ class Application(Tk):
         plot_button.grid(row=0, column=0, padx=5, pady=10, sticky="ew")
         back_button = ttk.Button(menu_frame, text="Back", cursor="right_ptr",
                                  command=lambda: [main.Application().mainloop()])
-        back_button.grid(row=2, column=0, padx=5, pady=10, sticky="ew")
-        auto_button = ttk.Button(menu_frame, text="Auto", cursor="right_ptr",
-                                 command=lambda: [self.foo()])
-        auto_button.grid(row=2, column=0, padx=5, pady=10, sticky="ew")
+        back_button.grid(row=1, column=0, padx=5, pady=10, sticky="ew")
+        toggle_pause_button = ttk.Button(menu_frame, text="Toggle pause", cursor="right_ptr",
+                                         command=lambda: [self.toggle_pause()])
+        toggle_pause_button.grid(row=2, column=0, padx=5, pady=10, sticky="ew")
+        self.step_entry = ttk.Entry(menu_frame, cursor="right_ptr")
+        self.step_entry.grid(row=3, column=0, padx=5, pady=10, sticky="ew")
 
         def f(event: Event, movement: int):
             """
@@ -77,6 +117,7 @@ class Application(Tk):
             :param event: information about the clic of the user (position and more)
             :param movement: information about the movement of the mouse
             """
+            print(event.x, event.y)
             if event.x <= self.pixel_start[0] or event.y <= self.pixel_start[1] or \
                     event.x >= self.pixel_end[0] or event.y >= self.pixel_end[1]:
                 pass
@@ -94,11 +135,8 @@ class Application(Tk):
                         pass
                     else:
                         self.past_value = (x_location, y_location)
-                        rectangle = plt.Rectangle((x_location, y_location), 1, 1, fc=self.color[
-                            (self.data[-y_location][x_location] + 1) % len(self.color)])
                         self.data[-y_location][x_location] = (self.data[-y_location][x_location] + 1) % len(self.color)
-                        self.ax.add_patch(rectangle)
-                        self.canvas.draw()
+                        self.im.set_data(self.data)
 
         def link_to_f_not_motion(event):
             f(event, 0)
@@ -111,61 +149,38 @@ class Application(Tk):
         self.ax = self.fig.add_subplot(111)
         self.ax.axes.get_xaxis().set_visible(False)
         self.ax.axes.get_yaxis().set_visible(False)
-        self.update_plt()
         self.tk.call("source", "azure.tcl")
         self.tk.call("set_theme", "light")
+
+        self.im = self.ax.imshow(self.data, cmap=self.cmap, norm=self.norm)
+        self.anim = animation.FuncAnimation(self.fig, self.animate, interval=self.interval, frames=200)
         tk.mainloop()
 
-    def update_data(self, x: int, y: int):
+    def update_data(self):
         """
         update_data is a function that update the data to one step
-
-        :param x: x position of the pixel
-        :param y: y position of the pixel
-        """
-        nb_neighbor = 0
-        for i in [-1, 0, 1]:
-            for j in [-1, 0, 1]:
-                if x + i < 0 or y + j < 0 or x + i > self.size[0] - 1 or y + j > self.size[1] - 1:
-                    pass
-                elif i == 0 and j == 0:
-                    pass
-                else:
-                    if self.data[x + i, y + j] == 2:
-                        nb_neighbor += 1
-        # (3 electron tail, 2 electron head, 1 conductor, 0 off)
-        if self.data[x, y] == 1:
-            self.data_update[x, y] = 1
-        if self.data[x, y] == 3:
-            self.data_update[x, y] = 1
-        if self.data[x, y] == 2:
-            self.data_update[x, y] = 3
-        if self.data[x, y] == 1 and (nb_neighbor == 2 or nb_neighbor == 1):
-            self.data_update[x, y] = 2
-
-    def update_plt(self):
-        """
-        update_plt update the canvas and the data
         """
         self.data_update = np.zeros(self.size)
-        self.ax.autoscale(enable=True, axis="x", tight=True)
-        self.ax.autoscale(enable=True, axis="y", tight=True)
+        for (x, y), value in np.ndenumerate(self.data):
+            match self.data[x, y]:
+                case self.CONDUCTOR:
+                    nb_neighbor = 0
+                    for i in [-1, 0, 1]:
+                        for j in [-1, 0, 1]:
+                            if x + i < 0 or y + j < 0 or x + i > self.size[0] - 1 or y + j > self.size[1] - 1:
+                                continue
+                            elif i == 0 and j == 0:
+                                continue
+                            else:
+                                if self.data[x + i, y + j] == self.ELECTRON_HEAD:
+                                    nb_neighbor += 1
+                    if nb_neighbor == 2 or nb_neighbor == 1:
+                        self.data_update[x, y] = self.ELECTRON_HEAD
+                    else:
+                        self.data_update[x, y] = self.CONDUCTOR
+                case self.ELECTRON_TAIL:
+                    self.data_update[x, y] = self.CONDUCTOR
+                case self.ELECTRON_HEAD:
+                    self.data_update[x, y] = self.ELECTRON_TAIL
+        return self.data_update
 
-        def color_pixel(state, x, y):
-            """
-            color_pixel update the canvas and the data
-
-            :param state: the value of the pixel (3 electron tail, 2 electron head, 1 conductor, 0 off)
-            :param x: x coordinate of the pixel
-            :param y: y coordinate of the pixel
-            :return: self.data_update the updated datas
-            """
-            rectangle = plt.Rectangle((y, -x), 1, 1, fc=self.color[state])
-            self.ax.add_patch(rectangle)
-
-        # Iterate over the elements and their indices using np.ndenumerate
-        for (line, element), value in np.ndenumerate(self.data):
-            self.update_data(line, element)
-        self.data = self.data_update
-        for (line, element), value in np.ndenumerate(self.data):
-            color_pixel(value, line, element)
