992693c5d5c93a53da9a21dfff779101717f1766
‘Téo <Teo.Bulteau@wi6labs.com>
2023-08-03 11:59:54 +0200

    Fix : brian brain

--- directory_brian_brain/brian_brain.py
+++ directory_brian_brain/brian_brain.py
@@ -2,22 +2,22 @@
 # -*- coding: utf-8 -*-
 # =============================================================================
 # Created By  : Bulteau Téo
-# Created Date: August 1 16:30:00 2023
+# Created Date: July 20 16:30:00 2023
 # For Wi6labs, all rights reserved
 # =============================================================================
 """The Module Has Been Build try zero player games"""
 # =============================================================================
 # Imports
+import main
+import matplotlib
+import numpy as np
+import tkinter as tk
+import matplotlib.pyplot as plt
 from tkinter import *
 from tkinter import ttk
-import matplotlib
-import tkinter as tk
-import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib import animation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import main
-import threading
-
 
 # ============================================================================
 
@@ -26,28 +26,64 @@ class Application(Tk):
     def __init__(self):
         Tk.__init__(self)  # Initialisation of the first window
         self.title("Brian's Brain")
-        self.color = {0: "white", 1: "black", 2: "red"}
+        self.color = ["white", "black", "red"]
+        self.step = 0
+        self.OFF = 0
+        self.ON = 1
+        self.DYING = 2
         self.size = (20, 20)
         self.pixel_start = (75, 72)
         self.pixel_end = (540, 534)
-        self.past_value = (2000, 2000)
         self.data = np.zeros(self.size)
         self.data_update = np.zeros(self.size)
+        self.step_entry = ttk.Entry()
         self.fig = matplotlib.figure.Figure
         self.ax = matplotlib.axes
         self.canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg
 
+        # Animation settings
+        self.paused = 1
+        self.interval = 100
+        self.im = matplotlib.image.AxesImage
+        self.anim = animation.FuncAnimation
+        self.cmap = colors.ListedColormap(self.color)  # ["white", "black"]
+        self.norm = colors.BoundaryNorm([0, 1, 2, 3], self.cmap.N)  # number of color HERE
+
         # Create widgets
         self.start_brian_brain()
 
-    def foo(self):
-        self.plot()
-        threading.Timer(0.25, self.foo).start()
+    def animate(self, i: int):
+        """
+        animate is the animation function
+        """
+        if self.paused:
+            pass
+        else:
+            self.upload_entry()
+            self.im.set_data(self.data)
+            self.data = self.update_data()
 
     def plot(self):
-        self.ax.clear()  # clear axes from previous plot
-        self.update_plt()
-        self.canvas.draw()
+        """
+        plot is a function to advance of one step in the simulation
+        """
+        self.upload_entry()
+        self.data = self.update_data()
+        self.im.set_data(self.data)
+
+    def upload_entry(self):
+        """
+        upload_entry is a function to add + 1 to the step counter
+        """
+        self.step -= -1
+        self.step_entry.delete(0, 2000)
+        self.step_entry.insert(0, "Step : " + str(self.step))
+
+    def toggle_pause(self):
+        """
+        toggle_pause is a function to start/stop the animation
+        """
+        self.paused = not self.paused
 
     def start_brian_brain(self):
         """
@@ -66,10 +102,12 @@ class Application(Tk):
         plot_button.grid(row=0, column=0, padx=5, pady=10, sticky="ew")
         back_button = ttk.Button(menu_frame, text="Back", cursor="right_ptr",
                                  command=lambda: [main.Application().mainloop()])
-        back_button.grid(row=2, column=0, padx=5, pady=10, sticky="ew")
-        auto_button = ttk.Button(menu_frame, text="Auto", cursor="right_ptr",
-                                 command=lambda: [self.foo()])
-        auto_button.grid(row=2, column=0, padx=5, pady=10, sticky="ew")
+        back_button.grid(row=1, column=0, padx=5, pady=10, sticky="ew")
+        toggle_pause_button = ttk.Button(menu_frame, text="Toggle pause", cursor="right_ptr",
+                                         command=lambda: [self.toggle_pause()])
+        toggle_pause_button.grid(row=2, column=0, padx=5, pady=10, sticky="ew")
+        self.step_entry = ttk.Entry(menu_frame, cursor="right_ptr")
+        self.step_entry.grid(row=3, column=0, padx=5, pady=10, sticky="ew")
 
         def f(event: Event, movement: int):
             """
@@ -91,23 +129,17 @@ class Application(Tk):
                         int(x_location) < 0 or int(y_location) <= -self.size[1]:
                     pass
                 else:
-                    if movement and self.past_value == (x_location, y_location):
+                    if movement == 1 and self.past_value == (x_location, y_location):
                         pass
                     else:
                         self.past_value = (x_location, y_location)
-                        if int(self.data[-y_location][x_location]):
-                            rectangle = plt.Rectangle((x_location, y_location), 1, 1, fc=self.color[0])
-                            self.data[-y_location][x_location] = 0
-                        else:
-                            rectangle = plt.Rectangle((x_location, y_location), 1, 1, fc=self.color[1])
-                            self.data[-y_location][x_location] = 1
-                        self.ax.add_patch(rectangle)
-                        self.canvas.draw()
+                        self.data[-y_location][x_location] = (self.data[-y_location][x_location] + 1) % len(self.color)
+                        self.im.set_data(self.data)
 
-        def link_to_f_not_motion(event: Event):
+        def link_to_f_not_motion(event):
             f(event, 0)
 
-        def link_to_f_motion(event: Event):
+        def link_to_f_motion(event):
             f(event, 1)
 
         self.bind("<Button-1>", link_to_f_not_motion)
@@ -115,58 +147,34 @@ class Application(Tk):
         self.ax = self.fig.add_subplot(111)
         self.ax.axes.get_xaxis().set_visible(False)
         self.ax.axes.get_yaxis().set_visible(False)
-        self.update_plt()
         self.tk.call("source", "azure.tcl")
         self.tk.call("set_theme", "light")
+
+        self.im = self.ax.imshow(self.data, cmap=self.cmap, norm=self.norm)
+        self.anim = animation.FuncAnimation(self.fig, self.animate, interval=self.interval, frames=200)
         tk.mainloop()
 
-    def update_data(self, x: int, y: int):
+    def update_data(self):
         """
         update_data is a function that update the data to one step
-
-        :param x: x position of the pixel
-        :param y: y position of the pixel
-        """
-        nb_neighbor = 0
-        for i in [-1, 0, 1]:
-            for j in [-1, 0, 1]:
-                if x + i < 0 or y + j < 0 or x + i > self.size[0] - 1 or y + j > self.size[1] - 1:
-                    pass
-                elif i == 0 and j == 0:
-                    pass
-                else:
-                    if self.data[x + i, y + j]:
-                        nb_neighbor += 1
-        if self.data[x, y] == 2:
-            self.data_update[x, y] = 0
-        if self.data[x, y] == 1:
-            self.data_update[x, y] = 2
-        if self.data[x, y] == 0 and nb_neighbor == 2:
-            self.data_update[x, y] = 1
-
-    def update_plt(self):
-        """
-        update_plt update the canvas and the data
         """
         self.data_update = np.zeros(self.size)
-        self.ax.autoscale(enable=True, axis="x", tight=True)
-        self.ax.autoscale(enable=True, axis="y", tight=True)
+        for (x, y), value in np.ndenumerate(self.data):
+            nb_neighbor = 0
+            for i in [-1, 0, 1]:
+                for j in [-1, 0, 1]:
+                    if x + i < 0 or y + j < 0 or x + i > self.size[0] - 1 or y + j > self.size[1] - 1:
+                        continue
+                    elif i == 0 and j == 0:
+                        continue
+                    else:
+                        if self.data[x + i, y + j]:
+                            nb_neighbor += 1
+            if self.data[x, y] == self.DYING:
+                self.data_update[x, y] = self.OFF
+            if self.data[x, y] == self.ON:
+                self.data_update[x, y] = self.DYING
+            if self.data[x, y] == self.OFF and nb_neighbor == 2:
+                self.data_update[x, y] = self.ON
+        return self.data_update
 
-        def color_pixel(state: int, x: int, y: int):
-            """
-            color_pixel update the canvas and the data
-
-            :param state: the value of the pixel (2 dying, 1 on, 0 off)
-            :param x: x coordinate of the pixel
-            :param y: y coordinate of the pixel
-            :return: self.data_update the updated datas
-            """
-            rectangle = plt.Rectangle((y, -x), 1, 1, fc=self.color[state])
-            self.ax.add_patch(rectangle)
-
-        # Iterate over the elements and their indices using np.ndenumerate
-        for (line, element), value in np.ndenumerate(self.data):
-            self.update_data(line, element)
-        self.data = self.data_update
-        for (line, element), value in np.ndenumerate(self.data):
-            color_pixel(value, line, element)
